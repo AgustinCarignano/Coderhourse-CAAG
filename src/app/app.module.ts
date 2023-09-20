66d8f31a0dc0724c511bd5de5c5c386f724dc524
Agustín Carignano <agustin.carignano@gmail.com>
2023-09-20 22:45:41 +0200

    creado layout general

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { ContentComponent } from './layout/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
